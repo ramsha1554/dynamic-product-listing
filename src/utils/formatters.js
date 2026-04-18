725a6b5a323c7b096e5e2a4e21bc09ef2c6e2a6d
ramsha132 <work.rm.13.dec@gmail.com>
2026-04-18 15:00:04 +0530

    Source: Add utility formatters

---
+++ src/utils/formatters.js
@@ -0,0 +1,11 @@
+export const formatCurrency = (amount) => {
+  return new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+  }).format(amount);
+};
+
+export const truncateText = (text, maxLength) => {
+  if (text.length <= maxLength) return text;
+  return text.substring(0, maxLength) + '...';
+};
